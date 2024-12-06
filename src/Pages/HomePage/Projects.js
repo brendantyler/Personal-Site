4bcb80092a14b819733e43de44d2bea8449096c6
Brendan Galman <galmanbrendan@gmail.com>
2024-12-06 01:15:51 -0600

    Add Project 2 Images and Project Desc

--- src/Pages/HomePage/Projects.js
+++ src/Pages/HomePage/Projects.js
@@ -21,30 +21,31 @@ function Projects() {
                 </div>
                 <div className="project">
                     <aside className="projectSidebar">
-                        <h3 className="projectName">Test Project 2</h3>
-                        <p className="projectDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        <h3 className="projectName">Koin Financial Tracker</h3>
+                        <p className="projectDesc">Koin is a simple Finance Application that keeps track of a users money, I will look to expand this project by adding predictive algorithms to help people budget and add graphing to better visualize data
+                        </p>
                     </aside>
                     <div className="projectImagesScroll">
                         <div className="projectImages">
-                            <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/FlightTyper_Start.png`} alt="Start Menu for a speed typing game" />
-                            <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/FlightTyper_Active.png`} alt="Gameplay of speed typing game that depicts a plane and text box" />
-                            <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/FlightTyper_End.png`} alt="Game Over Menu for a speed typing game" />
+                            <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/KoinMainLanding.png`} alt="Start Menu for a speed typing game" />
+                            <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/KoinFeatures.png`} alt="Gameplay of speed typing game that depicts a plane and text box" />
+                            <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/KoinDashMock.png`} alt="Game Over Menu for a speed typing game" />
                         </div>
                     </div>
                 </div>
-                <div className="project">
-                    <aside className="projectSidebar">
-                        <h3 className="projectName">Test Project 3</h3>
-                        <p className="projectDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-                    </aside>
-                    <div className="projectImagesScroll">
-                        <div className="projectImages">
-                            <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/FlightTyper_Start.png`} alt="Start Menu for a speed typing game" />
-                            <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/FlightTyper_Active.png`} alt="Gameplay of speed typing game that depicts a plane and text box" />
-                            <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/FlightTyper_End.png`} alt="Game Over Menu for a speed typing game" />
-                        </div>
-                    </div>
-                </div>
+            {/*    <div className="project">*/}
+            {/*        <aside className="projectSidebar">*/}
+            {/*            <h3 className="projectName">Test Project 3</h3>*/}
+            {/*            <p className="projectDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>*/}
+            {/*        </aside>*/}
+            {/*        <div className="projectImagesScroll">*/}
+            {/*            <div className="projectImages">*/}
+            {/*                <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/FlightTyper_Start.png`} alt="Start Menu for a speed typing game" />*/}
+            {/*                <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/FlightTyper_Active.png`} alt="Gameplay of speed typing game that depicts a plane and text box" />*/}
+            {/*                <img className="projectImage" src={`${process.env.PUBLIC_URL}/images/FlightTyper_End.png`} alt="Game Over Menu for a speed typing game" />*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
             </div>
         </section >
     );
